1ba6a7335b2f707e8a938105453cc405b89c93d9
Kiramman <kiramman89@gmail.com>
2023-10-08 16:01:46 +0700

    CsvSource

--- src/test/java/ru/netology/services/RestMonthCounterTest.java
+++ src/test/java/ru/netology/services/RestMonthCounterTest.java
@@ -2,23 +2,30 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RestMonthCounterTest {
-    @Test
-    public void testThreeMonthsRest() {
+public class RestMonthCounterTest {
+    @ParameterizedTest
+    @CsvSource({
+            "10000, 3000, 20000, 3",
+            "100000, 60000, 150000, 2"
+    })
+    public void testThreeMonthsRest(int income, int expenses, int threshold, int expected) {
         RestMonthCounter service = new RestMonthCounter();
-        int actual = service.calculate(10_000, 3_000, 20_000);
-        int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
+//        int expected = 3;
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void testTwoMonthsRest() {
-        RestMonthCounter service = new RestMonthCounter();
-        int actual = service.calculate(100_000, 60_000, 150_000);
-        int expected = 2;
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    public void testTwoMonthsRest() {
+//        RestMonthCounter service = new RestMonthCounter();
+//        int actual = service.calculate(100_000, 60_000, 150_000);
+//        int expected = 2;
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
